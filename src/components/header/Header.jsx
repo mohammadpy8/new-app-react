eae23b49bf116295e67d4620ac2fa836f7da0c9c
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-07 19:07:52 +0330

    dynamic-option

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
 import { MdLocationOn } from "react-icons/md";
-import { HiCalendar, HiSearch } from "react-icons/hi";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
 
 const Header = () => {
   const [destination, setDestination] = useState("");
+  const [openOption, setOpenOption] = useState(false);
+  const [option, setOption] = useState({
+    Adult: 1,
+    Children: 0,
+    Room: 2,
+  });
 
   return (
     <div className="header">
@@ -28,7 +34,10 @@ const Header = () => {
           <span className="seperator"></span>
         </div>
         <div className="headerSearchItem">
-          <div id="optionDropDown">1 adult &bull; 2 children &bull; 1 room</div>
+          <div id="optionDropDown" onClick={() => setOpenOption(!openOption)}>
+            1 adult &bull; 2 children &bull; 1 room
+          </div>
+          {openOption && <GuestOptionList option={option} />}
           <span className="seperator"></span>
         </div>
         <div className="headerSearchItem">
@@ -42,3 +51,30 @@ const Header = () => {
 };
 
 export default Header;
+
+const GuestOptionList = () => {
+  return (
+    <div className="guestOptions">
+      <OptionItem />
+      <OptionItem />
+      <OptionItem />
+    </div>
+  );
+};
+
+const OptionItem = () => {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">Adult</span>
+      <div className="optionCounter">
+        <button className="optionCounterBtn">
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">2</span>
+        <button className="optionCounterBtn">
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
