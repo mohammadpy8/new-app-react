3c0c924b2b82586f8298d6a79960fd2dded9b2cc
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-07 14:27:29 +0330

    new-styles-new-data

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 
-import AddContact from './components/AddContact';
-import ContactList from './components/ContactList';
-
-import { Routes, Route } from 'react-router-dom';
-
-import './App.css';
-import ContactDetails from './components/ContactDetails';
+import "./App.css";
 
 function App() {
-
-  const [contacts, setContacts] = useState([]);
-
-  const addContact = contact => {
-    
-    setContacts([...contacts, { id: crypto.randomUUID(), ...contact }]);
-  }; 
-
-  const removeContactHandler = (id) => {
-
-    const filteredContact = contacts.filter(contact => contact.id !== id);
-    setContacts(filteredContact)
-
-  };
-
-  useEffect(() => {
-
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-
-  }, [contacts]);
-
-  useEffect(() => {
-
-    const saveContacts = JSON.parse(localStorage.getItem("contacts"));
-
-    if (saveContacts) setContacts(saveContacts);
-
-  }, []);
-
-  return (
-    <main className='app'>
-      <h1>Contact App</h1>
-
-      <Routes>
-        <Route path='/add' element={<AddContact addContact={addContact} />} />
-        <Route path="/" element={<ContactList contactList={contacts} removeContactHandler={removeContactHandler} />} />
-        <Route path='/user/:id' element={<ContactDetails />} />
-      </Routes>
-
-    </main>
-  );
+  return <div></div>;
 }
 
 export default App;
