e7e398857516e4da70facc2ab48a64a4ac28756d
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-07 17:52:02 +0330

    searchBar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+
+const Header = () => {
+  const [destination, setDestination] = useState("");
+
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            type="text"
+            placeholder="where to go..."
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/06/22</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <div id="optionDropDown">1 adult &bull; 2 children &bull; 1 room</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
