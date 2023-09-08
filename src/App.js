161105791a275a2c4b30b89e6eab3378f962bbea
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-08 08:12:37 +0330

    fetch-data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./components/header/Header";
+import LocationList from "./components/LocationList/LocationList";
+
+import { Toaster } from 'react-hot-toast';
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Header />
+      <LocationList />
     </div>
   );
 }
