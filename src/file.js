2f7577ddb4557846cb02d52327e9ecd0a741c1d5
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-10 07:14:52 +0330

    file2

--- src/file.js
+++ src/file.js
@@ -3,22 +3,34 @@ const productItem = document.getElementsByClassName("pro");
 const buttons = document.querySelectorAll(".filter");
 
 const searchHandelr = (event) => {
-    const searchValue = event.target.value.toLowerCase().trim();
-    searchValue.array.forEach(product => {
-        const productName = product.chidren[1].innerText.toLowerCase();
-        if (productName.includes(searchValue)) {
-            product.style.display = "block";
-        } else {
-            product.style.display = "none";
-        }
-    });
+  const searchValue = event.target.value.toLowerCase().trim();
+  searchValue.array.forEach((product) => {
+    const productName = product.chidren[1].innerText.toLowerCase();
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
 };
 
 const filterHandler = (event) => {
-    const filter = event.target.dataset.filter;
-}
+  const filter = event.target.dataset.filter;
+
+  productItem.forEach((product) => {
+    const category = product.dataset.category;
+
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
 
 searchInput.addEventListener("keyup", searchHandelr);
-buttons.forEach(button => {
-    button.addEventListener("click", filterHandler);
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
 });
