9d21bfb7f14a89635633fff2a69c9bf2f058fdcb
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-10 07:43:35 +0330

    fix-file

--- src/file.js
+++ src/file.js
@@ -1,15 +1,17 @@
 const searchInput = document.getElementById("search-input");
 const productItem = document.getElementsByClassName("pro");
 const buttons = document.querySelectorAll(".filter");
+const priceDiv = document.getElementById("serch");
+const priceButton = priceDiv.querySelector("button");
 
 const changeClass = (filter) => {
-    buttons.forEach(button => {
-        if (button.dataset.filter === filter) {
-            button.classList.add("selected");
-        } else {
-            button.classList.remove("selected");
-        }
-    });
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
 };
 
 const searchHandelr = (event) => {
@@ -25,8 +27,8 @@ const searchHandelr = (event) => {
 };
 
 const filterHandler = (event) => {
-    const filter = event.target.dataset.filter;
-    changeClass(filter);
+  const filter = event.target.dataset.filter;
+  changeClass(filter);
 
   productItem.forEach((product) => {
     const category = product.dataset.category;
@@ -41,7 +43,27 @@ const filterHandler = (event) => {
   });
 };
 
-searchInput.addEventListener("keyup", searchHandelr);
-buttons.forEach((button) => {
-  button.addEventListener("click", filterHandler);
+const searchPriceHandler = (event) => {
+  const searchPrice = +event.target.parentElement.chidren[0].value;
+
+  productItem.forEach((product) => {
+    const productPrice = product.chidren[2].innerText;
+    const price = productPrice.split(" ")[1];
+
+    if (!searchPrice) {
+      product.style.display = "block";
+    } else {
+      searchPrice === price
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  searchInput.addEventListener("keyup", searchHandelr);
+  buttons.forEach((button) => {
+    button.addEventListener("click", filterHandler);
+  });
+  priceButton.addEventListener("click", searchPriceHandler);
 });
