f159cc1a1b596a5b01f86658441a5581959e50c2
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-14 15:03:30 +0330

    show-question

--- src/app/game.js
+++ src/app/game.js
@@ -2,11 +2,15 @@ import formatedData from "./helper";
 
 const loader = document.getElementById("");
 const container = document.getElementById("");
+const questionText = document.getElementById("");
+const answerList = document.getElementById("");
 
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -16,8 +20,24 @@ const fetchData = async () => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+const checkAnswer = () => {};
+
 window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
