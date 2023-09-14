cd30fc34f4f0728be9e050481fe7361866b43566
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-14 09:44:38 +0330

    quiz-app

---
+++ src/app/game.js
@@ -0,0 +1,21 @@
+const loader = document.getElementById("");
+const container = document.getElementById("");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+
+let formattedData = null;
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  formattedData = json;
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
